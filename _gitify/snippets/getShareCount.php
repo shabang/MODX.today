0759b83729610d1cc0385315faaf04e0da0adfc2
Mark Hamstra <hello@markhamstra.com>
2015-05-05 00:48:54 +0200

    Touch up share counts post

--- _gitify/snippets/getShareCount.php
+++ _gitify/snippets/getShareCount.php
@@ -8,11 +8,13 @@ $resourceId = $modx->getOption('resource', $_REQUEST, $modx->resource->get('id')
 $url = $modx->makeUrl($resourceId, '', '', 'full');
 $urlHash = md5($url);
 
+// Check if we have cached data first
 $cached = $modx->cacheManager->get('shares/' . $urlHash);
 if (is_array($cached)) {
     return $modx->toJSON($cached);
 }
 
+// No cache? Then this is where we'll grab the data.
 $urlEnc = urlencode($url);
 $total = 0;
 $results = array(
@@ -23,23 +25,21 @@ $results = array(
 // Facebook
 $fbUrl = "http://api.facebook.com/restserver.php?method=links.getStats&format=json&urls={$urlEnc}";
 $fbCount = reset($modx->fromJSON(file_get_contents($fbUrl)));
-// var_dump($fbCount);
 if (is_array($fbCount) && isset($fbCount['total_count'])) {
     $total += $fbCount['total_count'];
     $results['sources']['facebook'] = $fbCount['total_count'];
 }
 
-// Twitter - private endpoint
+// Twitter - this uses an undocumented endpoint which could break in the future
 $twitterUrl = "https://cdn.api.twitter.com/1/urls/count.json?url={$urlEnc}";
 $twitterCount = $modx->fromJSON(file_get_contents($twitterUrl));
-// var_dump($twitterCount);
 
 if (is_array($twitterCount) && isset($twitterCount['count'])) {
     $total += $twitterCount['count'];
     $results['sources']['twitter'] = $twitterCount['count'];
 }
 
-// Google+ - private endpoint
+// Google+ - this uses an undocumented endpoint which could break in the future
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, "https://clients6.google.com/rpc");
 curl_setopt($curl, CURLOPT_POST, 1);
@@ -50,6 +50,7 @@ $curl_results = curl_exec ($curl);
 curl_close ($curl);
 $json = json_decode($curl_results, true);
 
+// The number we're looking for is hidden deep!
 if (isset($json[0]['result']['metadata']['globalCounts']['count'])) {
     $total += $json[0]['result']['metadata']['globalCounts']['count'];
     $results['sources']['googleplus'] = $json[0]['result']['metadata']['globalCounts']['count'];
@@ -58,14 +59,14 @@ if (isset($json[0]['result']['metadata']['globalCounts']['count'])) {
 // LinkedIn
 $linkedinUrl = "https://www.linkedin.com/countserv/count/share?url={$urlEnc}&format=json";
 $linkedinCount = $modx->fromJSON(file_get_contents($linkedinUrl));
-// var_dump($linkedinCount);
-
 if (is_array($linkedinCount) && isset($linkedinCount['count'])) {
     $total += $linkedinCount['count'];
     $results['sources']['linkedin'] = $linkedinCount['count'];
 }
 
+// Grand total
 $results['total'] = $total;
 
+// Write it to cache
 $modx->cacheManager->set('shares/' . $urlHash, $results, 60 * 15);
 return $modx->toJSON($results);
