e525cfeaf6c16717e5da8890f012878a860ae971
Mark Hamstra <hello@markhamstra.com>
2015-05-25 11:37:32 +0200

    Release Robot Robbie updated Extra Updates for Week&nbsp;22 on Development
     * Release Robot Robbie updated Extra Updates for Week&nbsp;22
     * Release Robot Robbie updated Extra Updates for Week&nbsp;22

--- core/components/gitifywatch/model/gitifywatch/gitifywatch.class.php
+++ core/components/gitifywatch/model/gitifywatch/gitifywatch.class.php
@@ -138,7 +138,7 @@ Git::set_bin('/usr/local/bin/git');
 
             $remote = (!empty($environment['remote'])) ? $environment['remote'] : 'origin';
             $branch = (!empty($environment['branch'])) ? $environment['branch'] : $repo->active_branch();
-            $log['push'] = $repo->push($remote, $branch);
+            $log['push'] = $repo->push('origin', 'feature-extrasfeed');//$remote, $branch);
 
             $this->modx->log(modX::LOG_LEVEL_WARN, 'Auto-committing & pushing results: ' . print_r($log, true), '', __METHOD__, __FILE__, __LINE__);
 
